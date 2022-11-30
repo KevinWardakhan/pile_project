328917bf917183d538887ab8759d10e41e80fba3
legendevivante <118994255+legendevivante@users.noreply.github.com>
2022-11-30 15:09:56 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+char* valeur[]={"push","push#","ipush","pop","ipop","dup","op","jmp","jpz","rnd","read","write","call","ret","halt"};
+
+
+void traduction(char* nom_fichier){
+    FILE* fichier=NULL;
+    fichier=fopen(nom_fichier,'r');
+    int longueur=0
+    while ()
+    {
+        /* code */
+    }
+    
+    
+
+}
+int main(int argc, char const *argv[])
+{
+    char* nom_fichier=NULL;
+    nom_fichier=argv[1]
+
+    return 0;
+}
