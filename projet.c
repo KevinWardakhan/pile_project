c93e8f35780ce01ff036de5e4c6c182194328152
YacineAbdelouhab <118994777+YacineAbdelouhab@users.noreply.github.com>
2022-11-30 22:23:45 +0100

    Update projet.c

--- projet.c
+++ projet.c
@@ -5,6 +5,38 @@
 char* valeur[]={"push","push#","ipush","pop","ipop","dup","op","jmp","jpz","rnd","read","write","call","ret","halt"};
 
 
+int nombre_ligne_fichier(char* nom_fichier){
+/*--------Renvoie le nombre de fois où on trouve dans un certain fichier le caractère saut de ligne--------*/
+/* ----------------Attention ne renvoie pas le nombre de ligne visible par l'utilisateur-------------------*/
+/*Renvoie le nombre de fois où on a appuyer sur entrée pour sauter une ligne lors de la création du fichier*/
+
+  /*------------On verifiera si le fichier est bien ouvert--------------*/
+  /*Si problème d'ouverture, la fonction va renvoyer -1 pour le signaler*/
+  FILE* fichier=NULL;
+  fichier=fopen(nom_fichier,"r");
+  int cpt=0; /*Pour compter le nombre de ligne*/
+  char caractere_courant; /*Pour stocker le carcrère qu'on est en train de lire*/
+
+  if(fichier!=NULL){
+    while( (caractere_courant=fgetc(fichier) ) != EOF ){
+      if(caractere_courant == '\n'){
+        cpt++;
+      }
+    }
+  }
+    
+  else{
+    printf("Erreur Ouverture fichier.");
+    cpt=-2;
+  }
+  
+  fclose(fichier);
+  return cpt;
+}
+
+
+
+
 void traduction(char* nom_fichier){
     FILE* fichier=NULL;
     fichier=fopen(nom_fichier,'r');
